e7af69461acbb75599e9d6a2ee4eed8d31c631ee
MaulanaYusuf20 <maulanayusuf200101@gmail.com>
2023-11-25 19:45:39 +0700

    fix import controller

--- routes/transaction-route.js
+++ routes/transaction-route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { midtransChargeTransaction } = require("../controller/order-controller");
+const { midtransChargeTransaction } = require("../controller/transaction-controller");
 const route = express.Router();
 
 route.post("/charge", midtransChargeTransaction);
