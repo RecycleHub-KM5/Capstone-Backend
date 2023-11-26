d255686490344a5f21051724da9778af8b71caf3
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-11-26 23:14:00 +0700

    feat jwt verification

---
+++ controller/verify_jwt.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/configRoles.js');
+const User = require('../models').User;
+
+module.exports = {
+	verifyToken(req, res, next) {
+		let tokenHeader = req.headers['x-access-token'];
+
+		if (tokenHeader.split(' ')[0] !== 'Bearer') {
+			return res.status(400).send({
+				auth: false,
+				message: "Error",
+				errors: "Incorrect token format"
+			});
+		}
+
+		let token = tokenHeader.split(' ')[1];
+
+		if (!token) {
+			return res.status(403).send({
+				auth: false,
+				message: "Error",
+				errors: "No token provided"
+			});
+		}
+
+		jwt.verify(token, config.secret, (err, decoded) => {
+			if (err) {
+				return res.status(500).send({
+					auth: false,
+					message: "Error",
+					errors: err
+				});
+			}
+			req.userId = decoded.id;
+			next();
+		});
+	}
+}
