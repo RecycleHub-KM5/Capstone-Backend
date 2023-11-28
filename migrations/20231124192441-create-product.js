c6df3e25733397be6d7aeed98c95e80631f6b144
MaulanaYusuf20 <maulanayusuf200101@gmail.com>
2023-11-28 16:35:11 +0700

    add property thumbnail on product model

--- migrations/20231124192441-create-product.js
+++ migrations/20231124192441-create-product.js
@@ -1,34 +1,37 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('products', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      price: {
-        type: Sequelize.INTEGER
-      },
-      decription: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('products');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("products", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            price: {
+                type: Sequelize.INTEGER,
+            },
+            description: {
+                type: Sequelize.TEXT,
+            },
+            thumbnail: {
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("products");
+    },
 };
--- models/product.js
+++ models/product.js
@@ -1,25 +1,27 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class product extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  product.init({
-  name: DataTypes.STRING,
-  price: DataTypes.INTEGER,
-  decription: DataTypes.TEXT
-}, {
-  sequelize,
-  modelName: 'product',
-});
-return product;
+    product.init(
+        {
+            name: DataTypes.STRING,
+            price: DataTypes.INTEGER,
+            description: DataTypes.TEXT,
+            thumbnail: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            modelName: "product",
+        }
+    );
+    return product;
 };
