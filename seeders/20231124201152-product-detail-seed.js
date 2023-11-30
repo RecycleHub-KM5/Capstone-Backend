fb7fbf574a86d41eb954ec74b03e77417f204d93
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-11-30 11:48:18 +0700

    update detail product

--- seeders/20231124201152-product-detail-seed.js
+++ seeders/20231124201152-product-detail-seed.js
@@ -76,189 +76,189 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Full Color',
+      name: 'Pelangi Full Color',
       description:"Cocok ditempatkan di tembok sebagai pajangan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163651683931390025/produk4.png",
       id_product:4,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Monokrom',
+      name: 'Pelangi Monokrom',
       description:"Cocok ditempatkan di tembok sebagai pajangan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822363775443014/EEDE1712-BDF2-4D0F-87A1-3EC24EA187D6.jpg",
       id_product:4,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Pastel',
+      name: 'Pelangi Pastel',
       description:"Cocok ditempatkan di tembok sebagai pajangan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822363527991296/135B6C08-E8F0-4D46-A2FA-74B09D5D39E1.jpg",
       id_product:4,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Oren',
+      name: 'Burung Oren',
       description:"Hiasan rak atau meja yang berbahan kertas daur ulang",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163700505063731270/produk5.png",
       id_product:5,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Hijau',
+      name: 'Burung Hijau',
       description:"Hiasan rak atau meja yang berbahan kertas daur ulang",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822411686985769/F1FBD65B-A500-4352-9F49-CB5F2C1838CF.jpg",
       id_product:5,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Biru',
+      name: 'Burung Biru',
       description:"Hiasan rak atau meja yang berbahan kertas daur ulang",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822411078815755/29C25A8C-1206-44C9-B584-285C466CE7B2.jpg",
       id_product:5,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Putih',
+      name: 'Perahu Putih',
       description:"Cocok ditempatkan di tembok sebagai pajangan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822408914550825/277DAD79-47A4-4219-9B50-E07CD6F6B28B.jpg",
       id_product:6,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Baby Pink',
+      name: 'Perahu Baby Pink',
       description:"Cocok ditempatkan di tembok sebagai pajangan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822409765986334/0BEB253D-A287-433A-8A30-3BCC7D2CAE3E.jpg",
       id_product:6,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Hitam',
+      name: 'Perahu Hitam',
       description:"Cocok ditempatkan di tembok sebagai pajangan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822410462249010/A77A2C28-EF4D-416A-B558-53B9D1EF810B.jpg",
       id_product:6,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Putih',
+      name: 'Frame Putih',
       description:"Frame yang terbuat dari kertas daur ulang",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163700504614944788/produk7.png",
       id_product:7,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Abu-Abu',
+      name: 'Frame Abu-Abu',
       description:"Frame yang terbuat dari kertas daur ulang",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822364517847122/5C6C000B-489B-442E-B6F2-D9A171504FB6.jpg",
       id_product:7,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Hijau',
+      name: 'Frame Hijau',
       description:"Frame yang terbuat dari kertas daur ulang",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822364824010762/CEDF12E2-805B-426B-A9B8-99BEF9100C26.jpg",
       id_product:7,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Putih',
+      name: 'Pistol Putih',
       description:"barang daur ulang cocok menjadi teman bermain",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163700504359088189/produk8.png",
       id_product:8,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Merah',
+      name: 'Pistol Merah',
       description:"barang daur ulang cocok menjadi teman bermain",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822364094201856/D0BEB57D-A80E-4EF0-B988-E277029C0DE1.jpg",
       id_product:8,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Oren',
+      name: 'Pistol Oren',
       description:"barang daur ulang cocok menjadi teman bermain",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822365126021151/5DA69309-6024-4721-90C8-052EE4D5349B.jpg",
       id_product:8,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Putih',
+      name: 'Kipas Putih',
       description:"Kipas yang terbuat dari kertas daur ulang cocok untuk menyejukkan hari-hari anda",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701467216085052/produk9.png",
       id_product:9,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Kuning',
+      name: 'Kipas Kuning',
       description:"Kipas yang terbuat dari kertas daur ulang cocok untuk menyejukkan hari-hari anda",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701467216085052/produk9.png",
       id_product:9,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Biru',
+      name: 'Kipas Biru',
       description:"Kipas yang terbuat dari kertas daur ulang cocok untuk menyejukkan hari-hari anda",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701467216085052/produk9.png",
       id_product:9,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Biru',
+      name: 'Lampion Biru',
       description:"Lampion cantik yang terbuat dari kertas daur ulang. COcok ditempatkan di ruangan rumah anda",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701466939265075/produk10.png",
       id_product:10,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Kuning',
+      name: 'Lampion Kuning',
       description:"Lampion cantik yang terbuat dari kertas daur ulang. COcok ditempatkan di ruangan rumah anda",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701466939265075/produk10.png",
       id_product:10,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'oren',
+      name: 'Lampion oren',
       description:"Lampion cantik yang terbuat dari kertas daur ulang. COcok ditempatkan di ruangan rumah anda",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701466939265075/produk10.png",
       id_product:10,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Coklat',
+      name: 'Lampion Coklat',
       description:"Tempat tisu dari kardus daur ulang ramah lingkungan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701466595348480/produk11.png",
       id_product:11,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Hijau',
+      name: 'Tempat Tisu Hijau',
       description:"Tempat tisu dari kardus daur ulang ramah lingkungan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822412689428572/E8E2B1F4-E932-4D0B-ADF5-F4520BA47397.jpg",
       id_product:11,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Ungu',
+      name: 'Tempat Tisu Ungu',
       description:"Tempat tisu dari kardus daur ulang ramah lingkungan",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822412098019378/E224BB57-9D7C-4A06-90D4-ACFA62954A54.jpg",
       id_product:11,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Coklat',
+      name: 'Tempat Tisu Coklat',
       description:"Rumah kucing yang terbuat dari kardus daur ulang. Cocok bagi anda yang ingin menghemat biaya",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1163701466347864094/produk12.png",
       id_product:12,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Ungu',
+      name: 'Rumah Kucing Ungu',
       description:"Rumah kucing yang terbuat dari kardus daur ulang. Cocok bagi anda yang ingin menghemat biaya",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822413779935333/EBBB913B-C018-47B1-8E46-8A417655A83F.jpg",
       id_product:12,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
-      name: 'Hijau',
+      name: 'Rumah Kucing Hijau',
       description:"Rumah kucing yang terbuat dari kardus daur ulang. Cocok bagi anda yang ingin menghemat biaya",
       image: "https://cdn.discordapp.com/attachments/1153638653332029440/1164822413075300393/20250061-770B-40CF-8CA8-B5968EBFED8A.jpg",
       id_product:12,
