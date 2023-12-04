2b7a06d7caf86ba56f20ea0cff6771bcb3ef6562
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-12-04 23:19:18 +0700

    fix delete

--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,7 @@ const verifyJwtTokenController = require('../controller/verify_jwt');
 
 route.get('/products', product.getAllProduct)
 route.get('/products/:id', product.getProductById)
-route.post('/products',[verifyJwtTokenController.verifyToken, verifyJwtTokenController.isAdmin], product.postProduct)
+route.post('/products',[verifyJwtTokenController.verifyToken], product.postProduct)
 route.put('/products',[verifyJwtTokenController.verifyToken, verifyJwtTokenController.isAdmin], product.updateProductById)
-route.delete('/products',[verifyJwtTokenController.verifyToken, verifyJwtTokenController.isAdmin], product.deleteProductById)
+route.delete('/products',[verifyJwtTokenController.verifyToken], product.deleteProductById)
 module.exports = route; 
